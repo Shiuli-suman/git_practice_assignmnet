20d6321586f2a951baa3c5902b098b75fd890b38
Shiuli-suman <shiulisuman99@gmail.com>
2022-09-14 00:53:12 +0530

    Check Prime Number

---
+++ Problem_1.js
@@ -0,0 +1,14 @@
+// Check whether a number is Prime or not
+
+let num=13;
+let factor=0;
+for(let i=0;i<=num;i++){
+    if(num%i==0){
+        factor++;
+    }
+}
+if(factor!=2){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
